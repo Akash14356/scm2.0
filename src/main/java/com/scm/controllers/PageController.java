ead3424f9ea1c764b6c129d421ebe2ae800eec5e
Akash <akashgawali049@gmail.com>
2026-09-02 17:03:40 +0530

    Auto commit

---
+++ src/main/java/com/scm/controllers/PageController.java
@@ -0,0 +1,37 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+
+    //about route 
+    @RequestMapping("/about")
+    public String aboutPage(Model model){
+        model.addAttribute("isLogin", false);
+        System.out.println("About page Loading"); 
+        return "about";
+    }
+    @RequestMapping("/services")
+    public String servicesPage(){
+        System.out.println("Services page Loading"); 
+        return "services";
+    }
+
+
+    // @RequestMapping("/home")
+    // public String home(Model model){
+    //     System.out.println("Home page ");
+
+    //     //sending data to the view
+    //     model.addAttribute("name", "Akash");
+    //     model.addAttribute("youtubeChannel", "My YouTube Channel");
+    //     model.addAttribute("github", "github.com/akash");
+    //     model.addAttribute("myYoutubechannelLink","https://www.youtube.com/@AGEngineeringMantra");
+    //     return "home";
+    // }
+
+}
